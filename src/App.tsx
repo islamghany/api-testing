63018a22010c4e4df368446ea89d0d650ca52eed
Islam Mostafa <islamghany3@gmail.com>
2021-02-24 16:36:28 +0200

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const checkFetch= async  ()=>{
+    try{
+      const res = await fetch('http://localhost:5000');
+      const resData = await res.json();
+      console.log(resData);
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={checkFetch}>
+        check
+      </button>
     </div>
   );
 }
